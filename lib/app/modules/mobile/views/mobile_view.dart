d2dc911fa3fb7bb27e7169e370bc9904b3af13e5
iamrdsharma <rahulmvn13259@gmail.com>
2023-01-23 15:02:56 +0530

    Math Utils and required files

--- lib/app/modules/mobile/views/mobile_view.dart
+++ lib/app/modules/mobile/views/mobile_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter_application_00/app/modules/mobile/widget/yellow_button.dart';
 import 'package:flutter_application_00/app/modules/otp/views/otp_view.dart';
+import 'package:flutter_application_00/widgets/phone_text_field.dart';
 import 'package:flutter_application_00/widgets/snack_bar.dart';
 
 import 'package:get/get.dart';
@@ -52,43 +53,7 @@ class _MobileViewState extends State<MobileView> {
                 style: TextStyle(
                     fontSize: 20, color: Color(0xFF525252), height: 1.4),
               ),
-              TextFormField(
-                style: TextStyle(fontSize: 18, color: Color(0xFF999898)),
-                scrollPadding: EdgeInsets.symmetric(horizontal: 10),
-                keyboardType: TextInputType.number,
-                controller: numberController,
-                maxLength: 10,
-                decoration: InputDecoration(
-                    counterText: "",
-                    isDense: true,
-                    contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFFFEBA0F))),
-                    hintText: " Enter your mobile",
-                    hintStyle:
-                        TextStyle(fontSize: 18, color: Color(0xFF999898)),
-                    prefixIcon: Container(
-                      decoration: const BoxDecoration(
-                          border: Border(
-                              right: BorderSide(
-                                  width: 1, color: Color(0xFFCDCDCD)))),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          CountryCodePicker(
-                            onChanged: print,
-                            initialSelection: 'IN',
-                            favorite: ['+39', 'FR'],
-                            showCountryOnly: false,
-                            showOnlyCountryWhenClosed: false,
-                            alignLeft: false,
-                          ),
-                        ],
-                      ),
-                    ),
-                    border: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFFCDCDCD)))),
-              ),
+              MobileTextField(numberController),
               Row(
                 children: [
                   GestureDetector(
